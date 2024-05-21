e7c34c3e78df5bb864275b1da04b85ac9fb2db53
josegazano732 <josegazano7@outlook.com>
2024-05-20 23:16:16 -0300

    Inicio Rosita Pequeños Detalles

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+
+export class FooterComponent implements OnInit {
+
+  anio: number= new Date().getFullYear();
+
+
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
